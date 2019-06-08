179ab48e09c1a724ab20aebbad1aa82dbd3ae952
yhlleo <yahui.cvrs@gmail.com>
2019-06-08 20:28:19 +0200

    update

--- options/base_options.py
+++ options/base_options.py
@@ -60,6 +60,7 @@ class BaseOptions():
         parser.add_argument('--load_height', type=int, default=0, help='load image height')
         parser.add_argument('--use_augment', type=int, default=1, help='using data augmentation for training')
         parser.add_argument('--display_sides', type=int, default=1, help='displaying the side output results')
+        parser.add_argument('--num_classes', type=int, default=2, help='number of classes')
         self.initialized = True
         return parser
 
