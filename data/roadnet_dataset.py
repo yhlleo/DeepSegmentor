ccf2cfb75aa483a3dbb424074878ed2e2d9e44c7
yhlleo <yahui.cvrs@gmail.com>
2019-07-18 23:52:44 +0200

    update loader

--- data/roadnet_dataset.py
+++ data/roadnet_dataset.py
@@ -4,6 +4,7 @@ import os.path
 import random
 import cv2
 import glob
+import numpy as np
 from data.base_dataset import BaseDataset
 import torchvision.transforms as transforms
 from data.image_folder import make_dataset
