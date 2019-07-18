f2b4f9b1b35a693aa7ceb0c4f2d43a204ff537ed
yhlleo <yahui.cvrs@gmail.com>
2019-07-19 00:21:42 +0200

    update model

--- models/roadnet_model.py
+++ models/roadnet_model.py
@@ -76,7 +76,7 @@ class RoadNetModel(BaseModel):
         beta = count_neg/(count_neg+count_pos)
 
         pos_weight = beta / (1.0 - beta + 1e-6)
-        critic = torch.nn.BCEWithLogitsLoss(size_average=True, reduce=True, pos_weight=pos_weight)
+        critic = torch.nn.BCEWithLogitsLoss(reduction='elementwise_mean', reduce=True, pos_weight=pos_weight)
         return critic(logits, label)
 
     def forward(self):
