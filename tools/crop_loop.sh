97b4f4d9bf3c7eade252422dd13754ed498299e8
yhlleo <yahui.cvrs@gmail.com>
2019-07-18 22:44:21 +0200

    update tools

--- tools/crop_loop.sh
+++ tools/crop_loop.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-mkdir ../datasets/RoadNet
+dataroot=../datasets/Ottawa-Dataset
+save_folder=../datasets/RoadNet
 
 # train
-mkdir ../datasets/RoadNet/train_image
-mkdir ../datasets/RoadNet/train_segment
-mkdir ../datasets/RoadNet/train_edge
-mkdir ../datasets/RoadNet/train_centerline
+mkdir ${save_folder}/train_image
+mkdir ${save_folder}/train_segment
+mkdir ${save_folder}/train_edge
+mkdir ${save_folder}/train_centerline
 for i in `seq 2 15`
 do
-    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/Ottawa-$i.tif --save_path ../datasets/RoadNet/train_image
-    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/segmentation.png --save_path ../datasets/RoadNet/train_segment
-    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/edge.png --save_path ../datasets/RoadNet/train_edge
-    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/centerline.png --save_path ../datasets/RoadNet/train_centerline
+    python3 image_crop.py --image_file ${dataroot}/$i/Ottawa-$i.tif --save_path ${save_folder}/train_image
+    python3 image_crop.py --image_file ${dataroot}/$i/segmentation.png --save_path ${save_folder}/train_segment
+    python3 image_crop.py --image_file ${dataroot}/$i/edge.png --save_path ${save_folder}/train_edge
+    python3 image_crop.py --image_file ${dataroot}/$i/centerline.png --save_path ${save_folder}/train_centerline
 done
 
 # test
-mkdir ../datasets/RoadNet/test_image
-mkdir ../datasets/RoadNet/test_segment
-mkdir ../datasets/RoadNet/test_edge
-mkdir ../datasets/RoadNet/test_centerline
+mkdir ${save_folder}/test_image
+mkdir ${save_folder}/test_segment
+mkdir ${save_folder}/test_edge
+mkdir ${save_folder}/test_centerline
 for i in 1 16 17 18 19 20
 do
-    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/Ottawa-$i.tif --save_path ../datasets/RoadNet/test_image
-    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/segmentation.png --save_path ../datasets/RoadNet/test_segment
-    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/edge.png --save_path ../datasets/RoadNet/test_edge
-    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/centerline.png --save_path ../datasets/RoadNet/test_centerline
+    python3 image_crop.py --image_file ${dataroot}/$i/Ottawa-$i.tif --save_path ${save_folder}/test_image
+    python3 image_crop.py --image_file ${dataroot}/$i/segmentation.png --save_path ${save_folder}/test_segment
+    python3 image_crop.py --image_file ${dataroot}/$i/edge.png --save_path ${save_folder}/test_edge
+    python3 image_crop.py --image_file ${dataroot}/$i/centerline.png --save_path ${save_folder}/test_centerline
 done
