d6c3e14ee36809bb27a8f497e818c2ffc21d6b69
yhlleo <yahui.cvrs@gmail.com>
2019-07-18 22:27:40 +0200

    update tools

---
+++ tools/crop_loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for i in {2..15}
+do
+    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/Ottawa-$i.tif --save_path ../datasets/RoadNet/train_image
+    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/segmentation.png --save_path ../datasets/RoadNet/train_segment
+    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/edge.png --save_path ../datasets/RoadNet/train_edge
+    python3 image_crop.py --image_file ../datasets/Ottawa-Dataset/$i/centerline.png --save_path ../datasets/RoadNet/train_centerline
+done
