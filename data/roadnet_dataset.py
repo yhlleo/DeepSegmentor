20fa4408b86a26676b2c98ee7766877409fb8eeb
yhlleo <yahui.cvrs@gmail.com>
2019-07-18 23:52:03 +0200

    update loader

--- data/roadnet_dataset.py
+++ data/roadnet_dataset.py
@@ -46,7 +46,7 @@ class RoadNetDataset(BaseDataset):
         # read a image given a random integer index
         img_path = self.img_paths[index]
         image    = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-        image    = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        image    = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         # set paths of annotation maps
         segment_path    = os.path.join(self.segment_dir, os.path.basename(img_path))
