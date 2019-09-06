4d7175d5ed5391b09348c9bbe4011d19b3787e48
yhlleo <yahui.cvrs@gmail.com>
2019-09-06 11:23:05 +0200

    update

--- tools/guided_filter.py
+++ tools/guided_filter.py
@@ -18,15 +18,14 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
     results_dir  = os.path.join(args.results_dir, args.model_name, 'test_latest', 'images')
-    fused_list   = glob.glob(os.path.join(results_dir, '*{}.jpg'.format(args.suffix_fused)))
+    fused_list   = glob.glob(os.path.join(results_dir, '*{}.png'.format(args.suffix_fused)))
     sideout_list = [ll.replace(args.suffix_fused, args.suffix_sideout) for ll in fused_list]
 
     for ff, ss in zip(fused_list, sideout_list):
         img_fused = cv2.imread(ff, cv2.IMREAD_UNCHANGED)
         img_side  = cv2.imread(ss, cv2.IMREAD_UNCHANGED)
 
-        img_out   = np.zeros(img_fused.shape, dtype='uint8')
-        guidedFilter(img_fused, img_side, img_out, radius=5, eps=1e-6*255*255)
+        img_out = guidedFilter(img_fused, img_side, radius=5, eps=1e-6*255*255)
 
-        cv2.write(ff.replace(args.suffix_fused, args.suffix_output), img_gf, 
+        cv2.imwrite(ff.replace(args.suffix_fused, args.suffix_output), img_gf, 
             [cv2.IMWRITE_PNG_COMPRESSION, 0])
